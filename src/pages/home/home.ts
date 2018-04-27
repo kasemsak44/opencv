2a127005928dd48e65aabc4d8fee53e7c0d13ceb
Sierra Osx <macosx@Sierras-MacBook-Pro.local>
2018-04-27 15:30:42 +0700

    8

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AlertController } from 'ionic-angular';
 @Component({
+  
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+b1:any;
+b2:any;
+b3:any;
+b4:100;
+b5:any;
+  constructor(public navCtrl: NavController,public alerCtrl: AlertController) {
+    
 
   }
+  gotobas(){
+    this.navCtrl.push(HomePage);
+  }
+  GG(){if(this.b1<10000){
+  
+    this.b5 = this.b1*3/this.b4;
+    this.doAlert()
+  }
+  else{
+    this.b1>30000
+    this.b5 = this.b1*5/this.b4;
+    this.doAlert()
+  }}
+  
+  doAlert() {
+    let alert = this.alerCtrl.create({
+      title: 'ภาษี!',
+      message: 'ไม่ต้องจ่าย',
+      buttons: ['Ok']
+    });
+    alert.present()
+  
+  
+    }
+    dobas(){
+      let b5 = this.b1*5/this.b4;
+      return this.b5
+    }
+
+
+      
+
 
 }
+
+
